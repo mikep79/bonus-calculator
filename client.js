07898558ba83606fb324ae56dd798d73a4e9e273
Mike Pettman <mp_palmatum@yahoo.com>
2017-12-28 23:53:25 -0600

    added readme

--- client.js
+++ client.js
@@ -7,6 +7,56 @@ var mayella = { name: "Mayella", employeeNumber: "89068", annualSalary: "35000",
 
 var employees = [ atticus, jem, boo, scout, robert, mayella ];
 
-// YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
-console.log(employees);
+function EmployeeBonus (name, employeeNumber, annualSalary, reviewRating) {                 // EMPLOYEE BONUS CONSTRUCTOR
+    this.name = name;
+    this.bonusPercentage = bonusPercentageCalc(annualSalary, reviewRating, employeeNumber);
+    this.totalBonus = totalBonusCalc(annualSalary, this.bonusPercentage);
+    this.totalCompensation = totalCompensationCalc(annualSalary, this.totalBonus);
+}
+
+function bonusPercentageCalc(annualSalary, reviewRating, employeeNumber) {                  // CALCULATE BONUS PERCENTAGE
+    var bonusPercentage;
+    if (reviewRating <= 2){
+        bonusPercentage = 0;
+        return bonusPercentage;    
+    } else if (reviewRating === 3) {
+        bonusPercentage = 0.04;
+    } else if (reviewRating === 4) {
+        bonusPercentage = 0.06;
+    } else if (reviewRating === 5) {
+        bonusPercentage = 0.10;
+    }
+    if (employeeNumber.length === 4) {
+        bonusPercentage += 0.05;
+    }
+    if (annualSalary >= 65000) {
+        bonusPercentage -= 0.01;
+    }
+    return bonusPercentage;
+}
+
+function totalCompensationCalc(annualSalary, totalBonus) {                                      // CALCULATE TOTAL COMPENSATION
+    var totalCompensation = parseInt(annualSalary) + parseInt(totalBonus);
+    return totalCompensation;
+}
+
+function totalBonusCalc(annualSalary, bonusPercentage) {                                    // CALCULATE TOTAL BONUS
+    //console.log(annualSalary * bonusPercentage);
+    return annualSalary * bonusPercentage;
+}
+
+var bonusArray = [];                                                            // EMPLOYEE BONUS ARRAY
+
+for (var i = 0; i < employees.length; i++) {                                // add employees to bonusArray
+    bonusArray[i] = new EmployeeBonus(employees[i].name, employees[i].employeeNumber, employees[i].annualSalary, employees[i].reviewRating);
+}
+
+console.log(bonusArray);                                    // call finished bonusArray
+
+$('body').append('<p>test' + bonusArray[0][0] + '</p>');
+
+/* Problems:
+    - Numberic data is in string form. (use parseInt() for totalCompensation function).
+
+*/
